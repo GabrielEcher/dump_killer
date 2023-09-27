a7fb2213ea03392a6530a7cb375966d4986f89c9
Gabriel Ecker <gabrielecher14@gmail.com>
2023-09-27 16:35:31 -0300

    comentários explicando o código

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
 def dmp_killer():
-    folder_path = 'C://Cyberlog'
+    folder_path = '' # CAMINHO DA PASTA EM QUE OS MINIDUMPS SERÃO EXCLUÍDOS
 
-    for root, dirs, files in os.walk(folder_path):
+    for root, dirs, files in os.walk(folder_path): # PERCORRE A RAIZ, PASTAS, SUBPASTAS E ARQUIVOS
         for file_name in files:
-            if file_name.endswith('.dmp'):
-                file_path = os.path.join(root, file_name)
-                os.remove(file_path) 
+            if file_name.endswith('.dmp'): # VERIFICA A EXTENSÃO DO ARQUIVO
+                file_path = os.path.join(root, file_name) # FAZ UM JOIN COM A RAIZ DO ARQUIVO E O NOME DELE
+                os.remove(file_path) # EXCLUI O ARQUIVO
                            
 if __name__ == "__main__":
     dmp_killer()
